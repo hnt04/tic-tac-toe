9ff54f5b31d42dbbd84dd0ea7bfd346b21700131
hnt04 <105774543+hnt04@users.noreply.github.com>
2022-07-06 17:45:50 +0700

    update

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,7 +5,11 @@ export default function Board({ squares, handleClick }) {
   return (
     <div className="board">
       <div>
-        <div className="board-row">"Your code here"</div>
+        <div className="board-row">
+          <Square value={square[0] handleClick(()=>handleClick(0))}></Square>
+          <Square value = {square[1] handleClick(() => handleClick(1))}></Square>
+          <Square value = {square[2] handleClick(() => handleClick(2))}></Square>
+        </div>
         <div className="board-row">"Your code here"</div>
         <div className="board-row">"Your code here"</div>
       </div>
